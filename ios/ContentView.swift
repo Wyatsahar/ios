a92fd33ba890048879c77b88ea1359112ea4e606
xuchao <chao.xu@schillerchina.com>
2020-09-01 23:38:51 +0800

    Initial Commit

---
+++ ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios
+//
+//  Created by 许超 on 2020/9/1.
+//  Copyright © 2020 许超. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
